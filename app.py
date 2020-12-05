a42b25c797d28201051e2b7bf4306d45fb79d07c
Ebun O <remioludemi@gmail.com>
2020-12-05 15:51:27 -0500

    data

---
+++ app.py
@@ -0,0 +1,15 @@
+import flask
+import os
+
+app = flask.Flask(__name__)
+app.static_folder = 'static'
+
+@app.route('/')
+def artist_info(): 
+    return flask.render_template("index.html", test ="hello from templates")
+
+app.run(
+    port=int(os.getenv('PORT', 8080)),
+    host=os.getenv('IP', '0.0.0.0'),
+    debug=True
+)
